beeed89f5829fbe781511cdf3e6e183c63aa3b67
Anurag-R1974 <sanuragramagiri@gmail.com>
2025-04-28 22:34:31 +0530

    window made changes micro to nano 1st commit from windows

--- GitHubfile.py
+++ GitHubfile.py
@@ -1,1 +1,14 @@
+import boto3
 
+client = boto3.client('ec2', region_name='us-east-1')  # You can change region as needed
+
+response = client.run_instances(
+    ImageId='ami-00a929b66ed6e0de6',
+    InstanceType='t2.nano', #windows user created and changed to nano
+    KeyName='USKEY1',
+    MinCount=1,
+    MaxCount=1
+)
+
+print("EC2 Instance launched!")
+print(response['Instances'][0]['InstanceId'])
