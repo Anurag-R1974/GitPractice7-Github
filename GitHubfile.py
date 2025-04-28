2507ef20aa9ee7aced4b2b2eaf45f2e80f34977c
root <root@ip-172-31-81-170.ec2.internal>
2025-04-28 17:16:53 +0000

    Linux made changeanges ngesnges micro to large 1st commit by Linux

--- GitHubfile.py
+++ GitHubfile.py
@@ -1,1 +1,19 @@
+import boto3ii:q!
 
+
+
+
+client = boto3.client('ec2', region_name='us-east-1')  # You can change region as needed
+
+response = client.run_instances(
+    ImageId='ami-00a929b66ed6e0de6',
+    InstanceType='t2.large',
+    KeyName='USKEY1',
+    MinCount=1,
+    MaxCount=1
+)
+
+print("EC2 Instance launched!")
+
+print(response['Instances'][0]['InstanceId'])
+# linux created & changed micro to large
